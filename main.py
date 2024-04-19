aa98d0ac19744483e9cfdf07a519019bcba3db11
Narennnnn <mauryanarendra2003@gmail.com>
2024-04-19 15:51:16 +0545

    get put with error handling

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 # importing flask
 from flask import Flask
-from flask_restful  import Api,Resource
+from flask_restful  import Api,Resource,reqparse,abort
 
 
 # create instance of Flask
 app = Flask(__name__)
 api=Api(app)
-# route
-class sayHey(Resource):
-    def get(self):
-        return {"title":"hey!"}
-api.add_resource(sayHey,"/hey")
+
+video_put_args= reqparse.RequestParser()
+video_put_args.add_argument("name",type=str,help="Name of the video")
+video_put_args.add_argument("views",type=str,help="Views of the video")
+video_put_args.add_argument("likes",type=str,help="Likes on the video")
+videos={}
+
+def abortIfNotId(video_id):
+    if video_id not in videos:
+        abort(404,messae="Incorrect Id request")
+
+class video(Resource):
+    def get(self,video_id):
+        abortIfNotId(video_id)
+        return videos[video_id]
+    def put(self,video_id):
+        args=video_put_args.parse_args()
+        videos[video_id]=args
+        return videos[video_id],201
+api.add_resource(video,"/video/<int:video_id>")
 if __name__ == "__main__":
     app.run(debug=True)
--- test.py
+++ test.py
@@ -1,4 +1,9 @@
 import requests
-Base="http://127.0.0.1:5000/"
-response=requests.get(Base+"hey")
+
+Base = "http://127.0.0.1:5000/"
+# response = requests.put(Base + "video/1", data={"likes": "100k", "name": "Gangs of Wasseypur", "views": "90Million"})
+# print(response.json())
+# input()
+
+response=requests.get(Base+"video/2")
 print(response.json())
